feea586658b2f6d3a20cc28771ba1a4eae92db45
Mohammed Abdul Aziz <ma.azizweb@gmail.com>
2023-12-22 21:44:10 -0400

    Title Name Change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Mohammed Abdul Aziz';
+  title = 'Mohammed Abdul Aziz - Portfolio';
 }
