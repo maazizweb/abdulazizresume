d5679db83930b253fee46a952a0a770890f9a1aa
Mohammed Abdul Aziz <ma.azizweb@gmail.com>
2024-03-31 16:51:56 -0300

    Expeirences added and other api testing from excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { ApiServicesService } from '../services/api-services.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterModule,
+  ],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Mohammed Abdul Aziz - Portfolio';
+export class AppComponent implements OnInit{
+  title = 'Mohammed Abdul Aziz - Portfolios';
+   
+  constructor(private apiServ: ApiServicesService) {
+
+  }
+
+  ngOnInit() {
+
+ this.apiServ.getGoogleSheetData().then((res)=> {
+  console.log(res);
+  
+
+ })
+
+
+
+}
+
+
 }
---
+++ src/services/api-services.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiServicesService {
+
+  constructor(private http: HttpClient  ) {  }
+    // googleSheetApiUrl = "https://api.publicapis.org/entries"
+    googleSheetApiUrl = "https://script.google.com/macros/s/AKfycbwVwUCt7FNddEcYxj_Sdhb5T93-xY5I4YMpuf_QcLOiJCfHiiutyFFce1TDC9HL4WSJjA/exec"
+
+
+
+
+    public getGoogleSheetData(): Promise<any> {
+      return new Promise((resolve, reject) => {
+        this.http.get(this.googleSheetApiUrl).subscribe(
+          (response) => {
+            resolve(response);
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      });
+    }
+ 
+}
